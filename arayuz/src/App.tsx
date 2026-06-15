9de3981ba1d23d197f85a08bc280dcff6b11d96b
Hakan <108186126+hakanbakir@users.noreply.github.com>
2026-06-15 19:29:19 +0300

    v0.5.29: manuel tarih girince buton secimi kalkmasi

--- arayuz/src/App.tsx
+++ arayuz/src/App.tsx
@@ -323,7 +323,7 @@ function DashboardSayfasi({ onCariAc }: { onCariAc: (id: number) => void }) {
       if (o.sonIslemler !== undefined) setSonIslemler(o.sonIslemler);
       if (o.dSayfa) setDSayfa(o.dSayfa);
       if (o.dLimit) setDLimit(o.dLimit);
-      if (o.dPreset) setDPreset(o.dPreset);
+      if (o.dPreset !== undefined) setDPreset(o.dPreset);
       if (o.dSiralama) setDSiralama(o.dSiralama);
       if (o.baslangic) setBaslangic(o.baslangic);
       if (o.bitis) setBitis(o.bitis);
